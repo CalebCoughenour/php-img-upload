b210168281b989f8afa077d8a5d4a774b6f8302f
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-07 16:29:55 -0700

    add styling touchups

--- index.php
+++ index.php
@@ -6,8 +6,15 @@
   <div class="container">
     <div class="row">
       <div class="col-lg">
-        <div class="card">
-          <p>Hello Worlds!</p>
+        <div class="card" id="home-card">
+          <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <div class="row" id="upload-row">
+              <input type="file" name="file">
+            </div>
+            <div class="row">
+              <button type="submit" name="submit" class="upload-button">Upload</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
