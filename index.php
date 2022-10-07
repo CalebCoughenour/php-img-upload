bc2a70da9aaaa6eab93e704c87b8af5f1258536a
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-07 15:47:16 -0700

    initial project setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  require "header.php";
+?>
+
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg">
+        <div class="card">
+          <p>Hello Worlds!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php
+  require "footer.php";
+?>
